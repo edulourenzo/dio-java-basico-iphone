2bbbb33e4bdedd17f5eb141b1d71de7d4fb000d9
edulourenzo <edulourenzo@hotmail.com>
2024-10-12 16:59:26 -0300

    Instanciar classes na classe principal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        AparelhoTelefonico myAparelho = new AparelhoTelefonico();
+        NavegadorInternet myNavegador = new NavegadorInternet();
+        ReprodutorMusical myReprodutor = new ReprodutorMusical();
+
+        myAparelho.ligar("+55 (11) 9 9988-7766");
+        myNavegador.exibirPagina("www.dio.io");
+        myReprodutor.selecionarMusica("Pequena serenata noturna - Mozart");
     }
 }
