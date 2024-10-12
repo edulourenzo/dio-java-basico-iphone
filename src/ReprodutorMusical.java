11947797de02780b4db12b9acadb36d4efce14fc
edulourenzo <edulourenzo@hotmail.com>
2024-10-12 16:40:02 -0300

    Adicionar métodos da classe Reprodutor Musical

--- src/ReprodutorMusical.java
+++ src/ReprodutorMusical.java
@@ -1,4 +1,15 @@
 
 public class ReprodutorMusical extends IPhone {
 
+    public void tocar() {
+        System.out.println("Tocando a música!");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada!");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Musica " + musica + " selecionada");
+    }
 }
