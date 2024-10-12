cd6704bfd57d28dc60e5a50dc44d469cced1efc1
edulourenzo <edulourenzo@hotmail.com>
2024-10-12 16:47:11 -0300

    Adicionar métodos a classe Navegador de Internet

--- src/NavegadorInternet.java
+++ src/NavegadorInternet.java
@@ -1,4 +1,15 @@
 
 public class NavegadorInternet extends IPhone {
 
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba!");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página!");
+    }
 }
