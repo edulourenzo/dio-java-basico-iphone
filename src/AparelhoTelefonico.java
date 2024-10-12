7371f1c87172a9bdf8a4c317bba8c8767579fd5e
edulourenzo <edulourenzo@hotmail.com>
2024-10-12 16:44:20 -0300

    Adicionar métodos a classe Aparelho Telefónico

--- src/AparelhoTelefonico.java
+++ src/AparelhoTelefonico.java
@@ -1,4 +1,15 @@
 
 public class AparelhoTelefonico extends IPhone {
 
+    public void ligar(String numero) {
+        System.out.println("ligando parao núemero: " + numero);
+    }
+
+    public void atender() {
+        System.out.println("Atendendo ligação!");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz!");
+    }
 }
